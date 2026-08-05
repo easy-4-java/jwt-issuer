13ed5903e1ae1abdd852c63c9cdb2c1e01ac5ebf
wandl-6A72h <hiwepy@gmail.com>
2026-08-06 00:55:51 +0800

    feat(1.0.x): 添加 feature/1.0.x 特有的 VersionInfo 类

---
+++ jwt-issuer-api/src/main/java/io/github/easy4j/jwt/v100/VersionInfo100.java
@@ -0,0 +1,16 @@
+package io.github.easy4j.jwt.v100;
+
+/**
+ * Version identifier for jwt-issuer-api feature/1.0.x
+ * This class is unique to feature/1.0.x branch
+ */
+public final class VersionInfo100 {
+    
+    public static final String VERSION = "1.0.x.20260630-SNAPSHOT";
+    public static final String BRANCH = "feature/1.0.x";
+    public static final String BUILD_TIMESTAMP = "2026-08-06";
+    public static final String JAVA_TARGET = "1.8";
+    
+    private VersionInfo100() {
+    }
+}
